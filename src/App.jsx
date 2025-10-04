ec2ea19f751c57cec7c469cb6fe7488f20ae74c9
deimos715 <deimos715@gmail.com>
2025-10-04 20:33:43 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { StrictMode, Fragment, useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+    return (
+        <Fragment>
+            <Header />
+
+            <section>Тело страницы</section>
+
+            <Footer />
+        </Fragment>
+    );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <p>header</p>
+        </header>
+    );
+};
+
+export default Header;
